f50a80f7a19938f5cc50eb00e4538614f57cd726
prakashbtech87 <prakashbtech87@gmail.com>
2022-06-13 10:40:35 +0530

    Adding the project

---
+++ src/main/java/com/leetcode/LeetCodeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCodeProjectApplication.class, args);
+	}
+
+}
