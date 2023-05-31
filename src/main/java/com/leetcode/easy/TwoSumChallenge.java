86e4b53248b3811e2ca813e256a0a1a1a7a12b35
prakashk <prakashbtech87@gmail.com>
2023-05-31 20:04:38 +0530

    adding more

--- src/main/java/com/leetcode/easy/TwoSumChallenge.java
+++ src/main/java/com/leetcode/easy/TwoSumChallenge.java
@@ -2,6 +2,6 @@ package com.leetcode.easy;
 public class TwoSumChallenge {
 
     public static void main(String[] args) {
-        System.out.println("2 Sumzzzz");
+        System.out.println("2 Sum code");
     }
 }
